5b3b37c1583a11057b402c1f36c9973b6593f1a5
Bryan Romero <Bryanromerou@gmail.com>
2020-06-28 22:50:40 -0700

    Begining Html building

--- bugtracker/main/models.py
+++ bugtracker/main/models.py
@@ -6,6 +6,5 @@ class Bug(models.Model):
     bug_title = models.CharField(max_length = 200)
     bug_info = models.TextField()
     bug_published = models.DateTimeField('date published', default = datetime.now)
-
     def __str__(self):
         return self.bug_title
